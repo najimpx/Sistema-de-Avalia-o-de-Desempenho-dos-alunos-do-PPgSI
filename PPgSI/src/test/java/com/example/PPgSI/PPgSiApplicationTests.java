ff5b93ecf40be1a05a5cfeb7acc8fbecef2e51b0
RafaelNaj <91430231+najimpx@users.noreply.github.com>
2022-10-27 10:50:53 -0300

    Add files via upload

---
+++ PPgSI/src/test/java/com/example/PPgSI/PPgSiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PPgSI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PPgSiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
