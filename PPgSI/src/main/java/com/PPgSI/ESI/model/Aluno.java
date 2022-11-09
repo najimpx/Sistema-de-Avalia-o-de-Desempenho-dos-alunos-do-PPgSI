f7ce7850cef92c639fad087f7c4a263ff159ac82
RafaelNaj <91430231+najimpx@users.noreply.github.com>
2022-11-09 20:47:42 -0300

    Add files via upload

---
+++ PPgSI/src/main/java/com/PPgSI/ESI/model/Aluno.java
@@ -0,0 +1,104 @@
+package com.PPgSI.ESI.model;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.*;
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@Entity
+public class Aluno {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_aluno;
+    @Column
+    private Long nUSP ;
+    @Column
+    private String emailUSP;
+    @Column
+    private String nome;
+    @Column
+    private String link_curriculo_lattes;
+    @Column
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private Date data_entrada;
+    @Column
+    private String resultado_avaliacao_recente;
+    @Column
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private Date prazo_maximo_inscricao_programa_qualificacao;
+    @Column
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    private Date prazo_maximo_dissertacao;
+    
+    public Aluno() {
+    }
+    
+    public Long getId() {
+        return id_aluno ;
+    }
+    
+    public Long getnUSP() {
+        return nUSP ;
+    }
+
+    public String getEmailUSP() {
+        return emailUSP;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    
+    public String getLink_curriculo_lattes() {
+        return link_curriculo_lattes;
+    }
+    public Date getData_entrada() {
+        return data_entrada;
+    }
+    public String getResultado_avaliacao_recente() {
+        return resultado_avaliacao_recente;
+    }
+    public Date getPrazo_maximo_inscricao_programa_qualificacao() {
+        return prazo_maximo_inscricao_programa_qualificacao;
+    }
+    public Date getPrazo_maximo_dissertacao() {
+        return prazo_maximo_dissertacao;
+    }
+    
+    public void setId(Long id_aluno) {
+        this.id_aluno = id_aluno ;
+    }
+    
+    public void setnUSP(Long nUSP) {
+        this.nUSP = nUSP ;
+    }
+
+    public void setEmailUSP(String emailUSP) {
+    	this.emailUSP = emailUSP;
+    }
+
+    public void setNome(String nome) {
+    	this.nome = nome;
+    }
+    
+    public void setLink_curriculo_lattes(String link_curriculo_lattes) {
+        this.link_curriculo_lattes = link_curriculo_lattes;
+    }
+    public void setData_entrada(Date data_entrada) {
+        this.data_entrada = data_entrada;
+    }
+    public void setResultado_avaliacao_recente(String resultado_avaliacao_recente) {
+        this.resultado_avaliacao_recente = resultado_avaliacao_recente;
+    }
+    public void setPrazo_maximo_inscricao_programa_qualificacao(Date prazo_maximo_inscricao_programa_qualificacao) {
+        this.prazo_maximo_inscricao_programa_qualificacao = prazo_maximo_inscricao_programa_qualificacao;
+    }
+    public void setPrazo_maximo_dissertacao(Date prazo_maximo_dissertacao) {
+        this.prazo_maximo_dissertacao = prazo_maximo_dissertacao;
+    }
+}
