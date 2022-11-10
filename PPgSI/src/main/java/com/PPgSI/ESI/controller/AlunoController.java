53d7f6d51c653b322822e92d3ca076a5a84236b7
RafaelNaj <91430231+najimpx@users.noreply.github.com>
2022-11-10 00:33:29 -0300

    Add files via upload

--- PPgSI/src/main/java/com/PPgSI/ESI/controller/AlunoController.java
+++ PPgSI/src/main/java/com/PPgSI/ESI/controller/AlunoController.java
@@ -20,7 +20,7 @@ public class AlunoController {
 
     /**
      * <p>Retorna uma lista com todas as listas de plantas cadastradas no banco de dados.</p>
-     * Path: api/listafavoritos/listar
+     * Path: api/aluno/listar
      *
      * @return JSON com todas as listas cadastradas no banco de dados.
      */
@@ -31,9 +31,9 @@ public class AlunoController {
 
 
     /**
-     * <p>Cadastra uma nova planta no banco de dados. Seus dados (JSON) sao passados no
+     * <p>Cadastra um novo aluno no banco de dados. Seus dados (JSON) sao passados no
      * body da requisicao.</p>
-     * Path: api/planta/add
+     * Path: api/aluno/add
      *
      * @param planta Dados JSON da nova planta que sera cadastrada.
      */
@@ -45,9 +45,31 @@ public class AlunoController {
     }
 
 
+    
+    
+    @PutMapping(path = "/alterarDados/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Aluno> updateAluno(@PathVariable long id, @RequestBody Aluno dadosAluno) throws ResourceNotFoundException {
+    	Aluno alunoAlterado = repositorioAluno.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Planta com id '" + id + "' nao foi encontrado"));
+
+    	alunoAlterado.setnUSP(dadosAluno.getnUSP());
+    	alunoAlterado.setEmailUSP(dadosAluno.getEmailUSP());
+    	alunoAlterado.setNome(dadosAluno.getNome());
+    	alunoAlterado.setLink_curriculo_lattes(dadosAluno.getLink_curriculo_lattes());
+    	alunoAlterado.setTipo_de_curso(dadosAluno.getTipo_de_curso());
+    	alunoAlterado.setData_entrada(dadosAluno.getData_entrada());
+    	alunoAlterado.setResultado_avaliacao_recente(dadosAluno.getResultado_avaliacao_recente());
+        alunoAlterado.setPrazo_maximo_inscricao_programa_qualificacao(dadosAluno.getPrazo_maximo_inscricao_programa_qualificacao());
+        alunoAlterado.setPrazo_maximo_dissertacao(dadosAluno.getPrazo_maximo_dissertacao());
+
+        repositorioAluno.save(alunoAlterado);
+
+        return ResponseEntity.ok(alunoAlterado);
+    }
+    
     /**
-     * Remove uma lista cadastrada no banco de dados.
-     * Path: api/listafavoritos/remover/{id}
+     * Remove um aluno cadastrado no banco de dados.
+     * Path: api/aluno/remover/{id}
      *
      * @param id ID da lista que deve ser removida.
      */
